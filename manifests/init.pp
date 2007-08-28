82c8d85d1607197a35b9f91cd4b4ba065e5b4af8
root <root@puppetmaster.black.co.at>
2007-08-28 15:13:35 +0200

    Improve backuppc wakeup schedule, to run every half hour

--- manifests/init.pp
+++ manifests/init.pp
@@ -45,6 +45,9 @@ class backuppc::server {
 	backuppc::setting { PingMaxMsec: val => "40"; }
 	backuppc::setting { FullKeepCnt: val => "3"; }
 	backuppc::setting { BackupFilesExclude: val => '[ "\/proc", "\/sys", "\/backup", "\/media", "\/mnt", "\/var\/cache\/apt\/archives" ]' }
+	# wake up really often to catch intermittently connected hosts,
+	# wakeup first thing in the morning to do _nightly without disturbing too much
+	backuppc::setting { WakeupSchedule: val => '[ 4.25, 0..23, 0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5, 10.5, 11.5, 12.5, 13.5, 14.5, 15.5, 16.5, 17.5, 18.5, 19.5, 20.5, 21.5, 22.5, 23.5 ]' }
 	# TODO: collect backuppc_client definitions into hosts file
 }
 
