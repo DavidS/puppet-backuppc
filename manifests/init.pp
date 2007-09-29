f7f0203babac8ed1be7074a6ce4ad1f44b0cb3b9
root <root@puppetmaster.black.co.at>
2007-09-29 20:05:22 +0200

    big catchup commit for using hosting class for first domain
    * DNS update
    * hosting: add admin_user_email and customer parameter to hosting_vserver_configuration
    * dbp: update puppet_current package and surrounding infrastructure
    * dovecot: add a courier compatible namespace declaration to dovecot.conf
    * exim4: relay_from define and infrastructure to restructure relaying controls
    * exim4: fix real_mailname to usefqdn instead of hostname as fallback
    * exim4, hosting: move exim-gencert
    * exim4: fix test dirs
    * hosting: add virtual_hosts router
    * hosting: restructure configuration to configure the inside
      and outside of the vserver from a single definition
    * hosting: add separate logfile for the vhost
    * hosting: add exim4 authentification against ldap
    * hosting: add fstab entry to common data partition in the host
    * ssmtp: use new exim4::relay_from infrastructure
    * zones: moved entries for hosting migration around

--- manifests/init.pp
+++ manifests/init.pp
@@ -55,7 +55,8 @@ class backuppc::server {
 	backuppc::setting { WakeupSchedule: val => '[ 4.25, 0..23, 0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5, 10.5, 11.5, 12.5, 13.5, 14.5, 15.5, 16.5, 17.5, 18.5, 19.5, 20.5, 21.5, 22.5, 23.5 ]' }
 
 	# TODO: collect backuppc_client definitions into hosts file
-	File <<||>>
+	#File <<| tag == "backuppc::client" |>>
+	File <<| |>>
 }
 
 class backuppc::client {
